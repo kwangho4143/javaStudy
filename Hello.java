c591ab938c542f8927cbc2aa0e6325868818dea5
kwangho4143 <kwangho4143@naver.com>
2020-08-10 11:09:16 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to the java world!");
+		System.out.println("nice  you");
+		int myAge = 30;
+		System.out.println("My age is " + myAge);
+		
+		}
+}
