89e1a4664007a2dc05a083b469ddd47227ca2e6e
kwangho4143 <kwangho4143@naver.com>
2020-08-10 12:16:05 +0900

    second commit

---
+++ VariableExample.java
@@ -0,0 +1,8 @@
+public class VariableExample{
+	public static void main(String[] args){
+		int Value1 = 10;
+		int Value2 = 20;
+		int result = Value1+Value2;
+		System.out.println("10+20="+result);
+		}
+	}
